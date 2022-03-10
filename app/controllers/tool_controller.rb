fa97256b6cf3e5e2d4b4fa96204558e9ea4f7081
runrun0704 <satoru0704.f@gmail.com>
2022-03-10 23:37:21 +0900

    22/03/10 自分史システム部分完了

--- app/controllers/tool_controller.rb
+++ app/controllers/tool_controller.rb
@@ -17,7 +17,7 @@ class ToolController < ApplicationController
       flash[:notice] = "追加されました"
       redirect_to("/my_history/#{@m_h.id_pass}")
     else
-      flash[:notice] = "入力に誤りがあります"
+      flash[:notice] = "入力に誤りがあるか、IDがすでに存在しています"
       render("tool/my_history") 
     end  
   end
@@ -34,6 +34,22 @@ class ToolController < ApplicationController
       flash[:notice] = "見つかりませんでした"
       render("tool/my_history")
     end    
+  end  
+
+  def my_history_edit
+    @m_h = Datum.find_by(id_pass: params[:id_pass])
+  end  
+
+  def my_history_update
+    @m_h = Datum.find_by(id_pass: params[:id_pass])
+    @m_h.m_h_y = params[:m_h_y]
+    @m_h.m_h_s = params[:m_h_s]
+    @m_h.m_h_t = params[:m_h_t]
+    @m_h.m_h_k = params[:m_h_k]
+    @m_h.m_h_d = params[:m_h_d]
+    @m_h.save
+    flash[:notice] = "更新されました"
+    redirect_to("/my_history/#{@m_h.id_pass}")
   end  
   
   #モチベーショングラフ
@@ -65,7 +81,7 @@ class ToolController < ApplicationController
   end
 
   def motivation_edit
-
+    
   end
   
   def motivation_destroy
